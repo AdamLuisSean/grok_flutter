965b76897df659b1ebda918471298c0289c5c3f9
Alexey Shtanko <adamluissean@gmail.com>
2019-10-31 23:11:01 +0200

    Add example

--- lib/home.dart
+++ lib/home.dart
@@ -1,39 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:grokk_flutter/video_description.dart';
 
-/// Empty page widget fo developers that want to follow along with the tutorial
-/// on breaking down and implementing the Tik Tok UI (BLOG_LINK)
+import 'actions_toolbar.dart';
+import 'bottom_toolbar.dart';
+
+
 class Home extends StatelessWidget {
   Widget get topSection => Container(
-        height: 150,
-        color: Colors.yellow[300],
-      );
+    height: 100.0,
+    padding: EdgeInsets.only(bottom: 15.0),
+    alignment: Alignment(0.0, 1.0),
+    child: Row(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Text('Following'),
+          Container(
+            width: 15.0,
+          ),
+          Text('For you',
+              style: TextStyle(
+                  fontSize: 17.0, fontWeight: FontWeight.bold))
+        ]),
+  );
 
   Widget get middleSection => Expanded(
-        child: Row(
-          children: <Widget>[
-            Expanded(
-              child: Container(
-                color: Colors.green,
-              ),
-            ),
-            Container(
-              width: 100.0,
-              color: Colors.red,
-            )
-          ],
-        ),
-      );
-
-  Widget get bottomSection => Container(
-        height: 80,
-        color: Colors.blue,
-      );
+      child: Row(
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[VideoDescription(), ActionsToolbar()]));
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      children: <Widget>[topSection, middleSection, bottomSection],
-    ));
+      backgroundColor: Colors.black,
+      body: Column(
+        children: <Widget>[
+          // Top section
+          topSection,
+
+          // Middle expanded
+          middleSection,
+
+          // Bottom Section
+          BottomToolbar(),
+        ],
+      ),
+    );
   }
 }
+
